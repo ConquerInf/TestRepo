362229c26bebff5c4e3394ebe9f6dbd599e7e122
conquerInf <contourfree221@gmail.com>
2024-08-18 16:09:41 +0530

    add function to test

--- src/main/java/org/example/TestClass.java
+++ src/main/java/org/example/TestClass.java
@@ -1,7 +1,13 @@
 package org.example;
 
+import java.util.function.Function;
+
 public class TestClass {
     public static void main(String[] args) {
+        Function<String,String>function = s -> {
+            return "Hello "+s;
+        };
         System.out.println("This is a test class");
+        System.out.println("Manipulated String : "+function.apply("Contour"));
     }
 }
