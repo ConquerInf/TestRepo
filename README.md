26d9fe648d1728f300dff700188a2fe3207bb027
conquerInf <contourfree221@gmail.com>
2024-08-25 20:14:13 +0530

    add some changes

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# This is test repo for experimentation
+<style>
+
+r {background-color: red; font-weight:bold;}
+o {background-color: Orange}
+g {color: Green}
+bl {color: Blue; font-weight:bold;}
+bg {background-color: Yellow}
+</style>
+
+# <bg>This</bg> <o>is</o> <g>test</g> repo <r>for</r> <bl>experimentation</bl>
+
+<p>
+  <bl>This</bl> is some <bg>important</bg>
+</p>
--- src/main/java/org/example/TestClass.java
+++ src/main/java/org/example/TestClass.java
@@ -1,13 +1,35 @@
 package org.example;
 
+import java.util.List;
+import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 public class TestClass {
     public static void main(String[] args) {
-        Function<String,String>function = s -> {
-            return "Hello "+s;
+        Function<String, String> function = s -> {
+            return "Hello " + s;
         };
+        Predicate<String> predicate = s-> s.length()<8;
+        System.out.println("Testing Predicate : "+predicate.test("Hello Sir"));
         System.out.println("This is a test class");
-        System.out.println("Manipulated String : "+function.apply("Contour"));
+
+        Supplier<String> supplier = ()-> "Heello Supplier";
+        System.out.println("Testing supplier : "+supplier.get());
+
+        Consumer<String> consumer = (s)-> System.out.println("Consumer : "+s);
+        System.out.println("Testing consumer : ");
+        consumer.accept("Tesing consumer");
+        System.out.println("Manipulated String : " + function.apply("Contour"));
+
+        printNames(List.of("ankur", "kumar", "is", "a", "great", "coder"));
+    }
+
+    public static void printNames(List<String> names) {
+        names.forEach(name -> {
+                    System.out.println("Printing Names");
+                }
+        );
     }
 }
